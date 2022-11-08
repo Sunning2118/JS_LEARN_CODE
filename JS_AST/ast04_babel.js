af424b0768a59cb08ff9f28d53146699451f6b02
Sunning2118 <sunning2118@gmail.com>
2022-11-08 15:17:42 +0800

    update

--- JS_AST/ast04_babel.js
+++ JS_AST/ast04_babel.js
@@ -3,10 +3,10 @@ const parser = require("@babel/parser");
 const traverse = require("@babel/traverse");
 const { tsExternalModuleReference } = require("@babel/types");
 const types = require("@babel/types");
-function compile(code){
-    const ast=parser.parse(code)  // 生成抽象语法树
-    traverse.default(ast, {})  // 遍历转换代码
-    return generator.default(ast, {}, code)  // generator将AST转会成代码
+function compile(code) {
+  const ast = parser.parse(code)  // 生成抽象语法树
+  traverse.default(ast, {})  // 遍历转换代码
+  return generator.default(ast, {}, code)  // generator将AST转会成代码
 }
 const code = `
 function getData() {
--- JS_AST/ast05_babel.js
+++ JS_AST/ast05_babel.js
@@ -1,21 +1,29 @@
 const generator = require("@babel/generator");
 const parser = require("@babel/parser");
 const traverse = require("@babel/traverse");
-const types= require("@babel/types");
-function compile(code){
-    const ast=parser.parse(code)  // 生成抽象语法树
+const types = require("@babel/types");
+function compile(code) {
+    const ast = parser.parse(code)  // 生成抽象语法树
     const visitor = {  // 创建visitor对象
-        CallExpression(path){   // ES6特性函数 
-            const {callee}=path.node
+        CallExpression(path) {   // ES6特性函数 
+            const { callee } = path.node
             // path.node对象中的 callee 数据
-            const isConsoleLog=
-                types.isMemberExpression(callee)&&  
+            const isConsoleLog =
+                types.isMemberExpression(callee) &&  // MemberExpression通常指调用对象的成员，比如console对象调用log
                 // 成员表达式
-                callee.object.name==="console"&&
-                callee.property.name==="log";
-
+                callee.object.name === "console" &&
+                callee.property.name === "log";
+            if (isConsoleLog) {
+                const funcPath = path.findParent(p => {
+                    return p.isFunctionDelaration();
+                })
+                const funcName = funcPath.node.id.name;
+                path.node.arguments.unshift(types.stringLiteral(funcName))
+            }
         }
     }
+    traverse.default(ast, visitor)
+    return generator.default(ast, {}, code);
 }
 const code = `
 function getData() {
